ad3bc6274f182a8f56fc4b46555a8d04f6dbebdf
naveenbishwas <naveenbishwas4@gmail.com>
2024-08-07 23:56:46 +0530

    seo changes

--- src/app/Case-study/home-appliances/page.js
+++ src/app/Case-study/home-appliances/page.js
@@ -2,7 +2,10 @@ import React from "react";
 import Appliances from "./home-appliance";
 
 export const metadata = {
-  title: "Case-study/aplliances",
+  title:
+    "Transforming Ad Performance: Home Appliances India's Journey with Google Ads",
+  description:
+    "Explore how Home Appliances India turned initial Google Ads challenges into ecommerce success through strategic restructuring and budget management. This case study reveals the innovative solutions and remarkable outcomes achieved in their quest for digital advertising excellence",
 };
 
 export default function page() {
--- src/app/Case-study/travel/page.js
+++ src/app/Case-study/travel/page.js
@@ -2,7 +2,9 @@ import React from "react";
 import Travel from "./travel";
 
 export const metadata = {
-  title: "Case-study/travel",
+  title: "Scaling Success: E-sim's Record-Breaking Sales with Google Ads",
+  description:
+    "Discover how E-sim achieved phenomenal growth and outperformed competitors with strategic Google Ads campaigns. This case study highlights the journey from modest beginnings to record-breaking sales, showcasing the power of targeted advertising and optimization",
 };
 
 export default function page() {
