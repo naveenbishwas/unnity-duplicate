6bea955bcda4eff5d2ce8374a2fd59da8c695840
naveenbishwas <naveenbishwas4@gmail.com>
2024-08-10 13:55:43 +0530

    robot.tt

--- src/app/white-label-agency/white-label.js
+++ src/app/white-label-agency/white-label.js
@@ -129,40 +129,6 @@ const Label = () => {
                 style={{ width: "150px", height: "85px" }}
               ></Image>
             </div>
-
-            <nav>
-              <input type="checkbox" id="check" />
-              <label htmlFor="check">
-                <svg
-                  id="hamb-icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-list"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-                  />
-                </svg>
-              </label>
-              <ul>
-                <li>
-                  <Link href="#">About</Link>
-                </li>
-                <li>
-                  <Link href="#">Service</Link>
-                </li>
-                <li>
-                  <Link href="#">Contact</Link>
-                </li>
-                <li>
-                  <Link href="#">Sign up</Link>
-                </li>
-              </ul>
-            </nav>
           </div>
 
           <div className="front-banner-content">
